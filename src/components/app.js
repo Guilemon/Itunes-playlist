eb02c71443c23523881d62134f87439c9f2cf506
guilemon <kkanchika@gmail.com>
2018-05-23 19:54:26 +0530

    commiting basic working templates

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import HomePage from './homePage'
+import PopDetails from './popDetails'
+
+class App extends Component{
+    render(){
+        return (
+            <div><HomePage/></div>
+            )
+    }
+}
+
+export default App
---
+++ src/components/popDetails.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+let PopDetails = (props,details,toggle)=> {
+    const album = props.details
+
+    if(!album){
+        return "...Loading"
+    }
+    /*
+    ["im:releaseDate"].attribute.label
+    ["im:itemCount"].label
+    ["im:price"].label
+    link.attributes.href
+    rights.label
+    data["im:image"][2].label
+    category.attributes["im:id"]: "1264"
+    ​​​​label: "Tamil"
+    ​​​​scheme: "https://itunes.apple.com/in/genre/music-indian-tamil/id1264?uo=2"
+    term: "Tamil"
+    data["im:name"].label
+    data["im:artist"].label
+    */
+
+    return (
+        <div>
+        <div className="background-overlay" style={{backgroundImage:`url(${album["im:image"][2].label})`}}></div>
+        <div className="container">
+            <div className="row">
+            <div className="close-btn" onClick={props.toggle}>x</div>
+                <div className="col-md-6 col-sm-12">
+                    <img className="mx-auto d-block" src={album["im:image"][2].label}/>
+                </div>
+                <div className="col-md-6 col-sm-12">
+                    <h2 className="py-3">{album["im:name"].label}</h2>
+                    <h4 className="py-2">Artist: {album["im:artist"].label}</h4>
+                    <h5 className="py-2">Released on {album["im:releaseDate"].attributes.label}</h5>
+                    <h5 className="py-2">Total Track: {album["im:itemCount"].label}</h5>
+                    <h5 className="py-2"><a className="itunes-preview" href={album.link.attributes.href}>Preview on iTunes</a></h5>
+                    <h5 className="py-2">Price: {album["im:price"].label}</h5>
+                    <h5 className="py-2">Genre: {album.category.attributes.label}</h5>
+                    <h5 className="pt-5">{album.rights.label}</h5>
+                </div>
+            </div>
+        </div>
+        </div>
+        )
+    }
+
+export default PopDetails
